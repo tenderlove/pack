956d5ab38f51555181e839afdae7b0cdd7162144
Aaron Patterson <aaron.patterson@gmail.com>
2012-10-21 15:58:12 -0700

    we can apply n times

--- test/pack-test.scm
+++ test/pack-test.scm
@@ -4,3 +4,9 @@
 
 (test "A" (pack "C" (list 65)))
 (test "AB" (pack "CC" (list 65 66)))
+(test "AB" (pack "C2" (list 65 66)))
+(test "ABC" (pack "C3" (list 65 66 67)))
+;(test '(#\C #\C) (make-packer "CC"))
+;(test '(#\C 2) (make-packer "C2"))
+;(test '(#\C 21) (make-packer "C21"))
+;(test '(#\C 21 #\A) (make-packer "C21A"))
